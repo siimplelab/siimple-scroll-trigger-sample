d544bc2823c76d03ffb98904fc690c5aeb76754b
siimplelab <info@siimplelab.com>
2023-03-09 16:54:21 +0900

    scrollTrigger sample

---
+++ app.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let getRatio = el => window.innerHeight / (window.innerHeight + el.offsetHeight);
+
+gsap.utils.toArray("section").forEach((section, i) => {
+  section.bg = section.querySelector(".bg");
+
+  // Give the backgrounds some random images
+  section.bg.style.backgroundImage = `url(https://picsum.photos/1600/800?random=${i})`;
+  
+  // The first image (i === 0) should be handled differently because it should start at the very top
+  // Use function-based values in order to keep things responsive
+  gsap.fromTo(section.bg, {
+    backgroundPosition: () => i ? `50% ${-window.innerHiehgt * getRatio(section)}px` : "50% 0px"
+  }, {
+    backgroundPosition: () => `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+    ease: "none",
+    scrollTrigger: {
+      trigger: section,
+      start: () => i ? "top bottom" : "top top",
+      end: "bottom top",
+      scrub: true,
+      invalidateOnRefresh: true // to make it responsive
+    }
+  });
+});
